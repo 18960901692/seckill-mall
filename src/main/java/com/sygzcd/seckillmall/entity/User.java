7d5fc94da4b8e1d7d40732369852c9aa3f39216e
sygzcd <2969239608@qq.com>
2026-07-30 21:09:51 +0800

    fix:User实体实现Serializable接口

--- src/main/java/com/sygzcd/seckillmall/entity/User.java
+++ src/main/java/com/sygzcd/seckillmall/entity/User.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 /**
@@ -12,7 +13,9 @@ import java.time.LocalDateTime;
  */
 @Data
 @TableName("user")
-public class User {
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @TableId(type = IdType.AUTO)
     private Long id;
